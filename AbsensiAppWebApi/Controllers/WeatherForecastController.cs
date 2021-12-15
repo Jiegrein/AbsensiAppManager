5c317b1b019fc4e1253fcd7c2e076a43c8004adb
DANIEL ABEDNEGO <daniel.abednego@project.accelist.com>
2021-12-15 17:31:51 +0700

    fix

--- AbsensiAppWebApi/Controllers/WeatherForecastController.cs
+++ AbsensiAppWebApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AbsensiAppWebApi.API;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
