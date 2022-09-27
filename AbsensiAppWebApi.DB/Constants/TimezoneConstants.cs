cce527bf3f5bae6b9e94e02548ff19fb8e069158
DANIEL ABEDNEGO <daniel.abednego@creditenable.com>
2022-09-27 11:18:40 +0700

    fix timezone issues

---
+++ AbsensiAppWebApi.DB/Constants/TimezoneConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsensiAppWebApi.DB.Constants
+{
+    public class TimezoneConstants
+    {
+        public const string WibTime = "SE Asia Standard Time";
+        public const string WitaTime = "Singapore Standard Time";
+        public const string WitTime = "Tokyo Standard Time";
+    }
+}
